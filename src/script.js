fddd97fed759f2b1f13f77a29170c414c5838e99
subikstha <subsatch@gmail.com>
2025-11-28 20:58:53 +0545

    Added axes helper to the scene

--- src/script.js
+++ src/script.js
@@ -15,18 +15,30 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({
   color: 0xff0000,
-  wireframe: true,
+  wireframe: false,
 });
 const mesh = new THREE.Mesh(geometry, material);
+// Position of the mesh can be changed right before adding the mesh to the scene
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+// mesh.position.x = 0.7;
+// Setting all x,y and z at once
+mesh.position.set(0.7, -0.6, 1);
 
 scene.add(mesh);
-
+console.log("logging mesh", mesh.position.length()); // gives the position of the mesh from the center
 // Camera
 
+// Axis helper
+const axesHelper = new THREE.AxesHelper();
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+console.log("Distance to camera", mesh.position.distanceTo(camera.position));
 // camera.position.x = 1;
 camera.position.z = 2;
+camera.position.y = 1;
+camera.position.x = 1;
 scene.add(camera);
+scene.add(axesHelper);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
